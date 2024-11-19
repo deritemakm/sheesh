0925bf3d765845b2fec60cf0ee22fd8f0fe11f2d
Kris-Amor <kamorstudy@gmail.com>
2024-11-20 01:48:25 +0800

    Messing around LOL

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ const char *keywords[] = {
     "group", "iffy", "input", "jump", "legit", "locked", "lockin",
     "long", "meanwhile", "nickname", "num", "open", "other", "out",
     "outside", "pl", "rep", "scenario", "short", "standard", "stop",
-    "team", "text", "vibe", NULL
+    "team", "text", "vibe", "cap", "nocap", NULL
 };
 
 const char *reservedWords[] = { "always", "cap", "cont", "nocap", "toptier", NULL };
